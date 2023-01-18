f19a6515ab5e7b700b599e1b3d35406787c2b931
Josue Victor <victorarroxellas01@gmail.com>
2023-01-17 23:23:36 -0300

    first commit

---
+++ js/home.js
@@ -0,0 +1,27 @@
+const currentDate = document.querySelector('p.containerCurrentDate')
+
+const textcurrentDay = new Date().getDay()
+const textcurrentDate = new Date().getDate()
+const textcurrentMonth = new Date().getMonth() + 1
+const textcurrentYear = new Date().getFullYear()
+
+switch (textcurrentDay) {
+  case 0:
+    currentDate.append('Domingo');
+    break;
+  case 1:
+    currentDate.append('segunda');
+    break;
+  case 2:
+    currentDate.append(`Terça - ${textcurrentDate}/${textcurrentMonth}/${textcurrentYear}`);
+    break;
+  case 3:
+    currentDate.append('Quarta')
+    break;
+
+  default:
+    break;
+}
+
+
+
