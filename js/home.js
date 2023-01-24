118cb662fac81a11567ab8d063e61a87a3eebd02
Josue <victorarroxellas01@gmail.com>
2023-01-24 17:27:43 -0300

    calendario

--- js/home.js
+++ js/home.js
@@ -1,39 +1,36 @@
-/*Data*/
-
-const lista = [];
-
-const currentDate = document.querySelector('p.containerCurrentDate')
-
-const textcurrentDay = new Date().getDay()
-const textcurrentDate = new Date().getDate()
-const textcurrentMonth = new Date().getMonth() + 1
-const textcurrentYear = new Date().getFullYear()
-
-switch (textcurrentDay) {
-  case 0:
-    currentDate.append('Domingo');
-    break;
-  case 1:
-    currentDate.append('segunda');
-    break;
-  case 2:
-    currentDate.append(`Terça - ${textcurrentDate}/${textcurrentMonth}/${textcurrentYear}`);
-    break;
-  case 3:
-    currentDate.append(`Quarta - ${textcurrentDate}/${textcurrentMonth}/${textcurrentYear}`);
-  break;
-
-  default:
-    break;
-};
-
-/*Evento btn AddEvent*/
 
 
-const DOM = {
-  showModalAddEvent() {
-    alert('clicou aqui')
-  }
-  
-}
 
+// const currentDate = document.querySelector('p.containerCurrentDate')
+
+// const textcurrentDay = new Date().getDay()
+// const textcurrentDate = new Date().getDate()
+// const textcurrentMonth = new Date().getMonth() + 1
+// const textcurrentYear = new Date().getFullYear()
+
+// switch (textcurrentDay) {
+//   case 0:
+//     currentDate.append(`Domingo - ${textcurrentDate}/${textcurrentMonth}/${textcurrentYear}`);
+//     break;
+//   case 1:
+//     currentDate.append(`segunda - ${textcurrentDate}/${textcurrentMonth}/${textcurrentYear}`);
+//     break;
+//   case 2:
+//     currentDate.append(`Terça - ${textcurrentDate}/${textcurrentMonth}/${textcurrentYear}`);
+//     break;
+//   case 3:
+//     currentDate.append(`Quarta - ${textcurrentDate}/${textcurrentMonth}/${textcurrentYear}`);
+//   break;
+//   case 4:
+//     currentDate.append(`Quinta - ${textcurrentDate}/${textcurrentMonth}/${textcurrentYear}`);
+//   break;
+//   case 5:
+//     currentDate.append(`Sexta - ${textcurrentDate}/${textcurrentMonth}/${textcurrentYear}`);
+//   break;
+//   case 6:
+//     currentDate.append(`Sábado - ${textcurrentDate}/${textcurrentMonth}/${textcurrentYear}`);
+//   break;
+
+//   default:
+//     break;
+// };
